ed109442e78fb980306134cb2b5870bc0f1db7e7
Nathan Adams <dinnerbone@dinnerbone.com>
2016-04-10 00:30:46 +0200

    Store permissions with lowercase users & channels, and make lookups case insensitive

--- core/permissions.js
+++ core/permissions.js
@@ -5,34 +5,56 @@ module.exports = (bot, config) => {
         config.data.permissions = {};
     }
     const users = config.data.permissions;
+    const getUser = (name) => {
+        name = name.toLowerCase();
+        let result = users[name];
+        if (typeof result === 'undefined') {
+            result = {admin: false, channels: []};
+            users[name] = result;
+        }
+        return result;
+    };
+    const saveConfig = () => {
+        for (const name in users) {
+            const user = users[name];
+            if (!user.admin && user.channels.length === 0) {
+                delete users[name];
+            }
+        }
+        return config.save();
+    };
+
+    for (const name in users) {
+        const user = users[name];
+        if (typeof user.admin !== 'boolean') {
+            user.admin = false;
+        }
+        if (!Array.isArray(user.channels)) {
+            user.channels = [];
+        }
+    }
+
     return {
-        isAdmin: (user) => {
-            return Boolean(users[user] && users[user].admin);
+        isAdmin: (name) => {
+            return getUser(name).admin;
         },
-        isOperator: (user, channel) => {
-            return Boolean(users[user] && users[user].channels && users[user].channels.indexOf(channel) > -1);
+        isOperator: (name, channel) => {
+            return getUser(name).channels.indexOf(channel.toLowerCase()) > -1;
         },
-        setAdmin: (user, value) => {
-            if (typeof users[user] === 'undefined') {
-                users[user] = {};
-            }
-            users[user].admin = !!value;
-            return config.save();
+        setAdmin: (name, value) => {
+            const user = getUser(name);
+            user.admin = !!value;
+            return saveConfig();
         },
-        setOperator: (user, channel, value) => {
-            if (typeof users[user] === 'undefined') {
-                users[user] = {};
-            }
-            if (typeof users[user].channels === 'undefined') {
-                users[user].channels = [];
-            }
-            const index = users[user].channels.indexOf(channel);
+        setOperator: (name, channel, value) => {
+            const user = getUser(name);
+            const index = user.channels.indexOf(channel.toLowerCase());
             if (value && index === -1) {
-                users[user].channels.push(channel);
-                return config.save();
+                user.channels.push(channel.toLowerCase());
+                return saveConfig();
             } else if (!value && index > -1) {
-                users.splice(index, 1);
-                return config.save();
+                user.channels.splice(index, 1);
+                return saveConfig();
             }
         }
     };
