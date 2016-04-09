531b44a65e8850d2643cf94422e46af212760e27
Nathan Adams <dinnerbone@dinnerbone.com>
2016-04-10 01:12:11 +0200

    Some more small refactoring in op_commands.js

--- plugins/op_commands.js
+++ plugins/op_commands.js
@@ -14,7 +14,7 @@ module.exports = (bot, config) => {
     };
 
     function addTimer(time, action, target, channel) {
-        let timer = {
+        const timer = {
             time: time,
             action: action,
             target: target,
@@ -26,7 +26,7 @@ module.exports = (bot, config) => {
     }
 
     function removeTimer(timer) {
-        let location = timers.indexOf(timer);
+        const location = timers.indexOf(timer);
         timers.splice(location, 1);
         config.save();
     }
@@ -51,15 +51,15 @@ module.exports = (bot, config) => {
                 return moment().add(args[i], unit);
 
             } else if (/^\d+\w$/.test(args[i])) {
-                let time = args[i].replace(/[^\d]/gi, '');
-                let unit = args[i].replace(/[\d]/gi, '');
+                const time = args[i].replace(/[^\d]/gi, '');
+                const unit = args[i].replace(/[\d]/gi, '');
                 return moment().add(time, moment.normalizeUnits(unit));
             }
         }
     }
 
     // Load timers on plugin load
-    for (let timer of timers) {
+    for (const timer of timers) {
         let time = moment(timer.time).diff(moment());
         time = Math.max(time, 15 * 1000); // Timeout to make sure the bot is connected.
         setTimeout(executeTimer, time, timer);
@@ -72,103 +72,103 @@ module.exports = (bot, config) => {
                 {
                     pattern: /^(\S*)$/,
                     requires: 'operator',
-                    execute: (event, user) => {
-                        if (user.length === 0) user = event.nick;
-                        bot.giveOp(user, event.channel)
+                    execute: (event, target) => {
+                        if (target.length === 0) target = event.nick;
+                        bot.giveOp(target, event.channel)
                             .then(() => {
-                                bot.notify(event.nick, `User ${user} was opped.`);
+                                bot.notify(event.nick, `${target} was opped.`);
                             })
                             .catch((error) => {
-                                bot.notify(event.nick, `Could not op user: ${error}`);
+                                bot.notify(event.nick, `Could not op ${target}: ${error}`);
                             });
                     }
                 },
                 {
                     pattern: /^(\S*) (\S+) *(\S*)$/,
                     requires: 'operator',
-                    execute: (event, user) => {
-                        if (user.length === 0) user = event.nick;
+                    execute: (event, target) => {
+                        if (target.length === 0) target = event.nick;
                         const time = parseTime(event.args);
-                        bot.giveOp(user, event.channel)
+                        bot.giveOp(target, event.channel)
                             .then(() => {
-                                addTimer(time, 'deop', user, event.channel);
+                                addTimer(time, 'deop', target, event.channel);
 
-                                bot.notify(event.nick, `User ${user} was opped for ${time.toNow(true)}.`);
+                                bot.notify(event.nick, `${target} was opped for ${time.toNow(true)}.`);
                             })
                             .catch((error) => {
-                                bot.notify(event.nick, `Could not op user: ${error}`);
+                                bot.notify(event.nick, `Could not op ${target}: ${error}`);
                             });
                     }
                 },
-                'Usage: op [USER_NAME] [TIME_STAMP]'
+                'Usage: op TARGET_NICK [TIME_STAMP]'
             ],
             deop: [
                 {
                     pattern: /^(\S*)$/,
                     requires: 'operator',
-                    execute: (event, user) => {
-                        if (user.length === 0) user = event.nick;
-                        bot.takeOp(user, event.channel)
+                    execute: (event, target) => {
+                        if (target.length === 0) target = event.nick;
+                        bot.takeOp(target, event.channel)
                             .then(() => {
-                                bot.notify(event.nick, `User ${user} was deopped.`);
+                                bot.notify(event.nick, `${target} was deopped.`);
                             })
                             .catch((error) => {
-                                bot.notify(event.nick, `Could not deop user: ${error}`);
+                                bot.notify(event.nick, `Could not deop ${target}: ${error}`);
                             });
                     }
                 },
-                'Usage: deop [USER_NAME]'
+                'Usage: deop [TARGET_NICK]'
             ],
             voice: [
                 {
                     pattern: /^(\S*)$/,
                     requires: 'operator',
-                    execute: (event, user) => {
-                        if (user.length === 0) user = event.nick;
-                        bot.giveVoice(user, event.channel)
+                    execute: (event, target) => {
+                        if (target.length === 0) target = event.nick;
+                        bot.giveVoice(target, event.channel)
                             .then(() => {
-                                bot.notify(event.nick, `User ${user} was voiced.`);
+                                bot.notify(event.nick, `${target} was voiced.`);
                             })
                             .catch((error) => {
-                                bot.notify(event.nick, `Could not voice user: ${error}`);
+                                bot.notify(event.nick, `Could not voice ${target}: ${error}`);
                             });
                     }
                 },
                 {
                     pattern: /^(\S*) (\S+) *(\S*)$/,
                     requires: 'operator',
-                    execute: (event, user) => {
-                        if (user.length === 0) user = event.nick;
+                    execute: (event, target) => {
+                        if (target.length === 0) target = event.nick;
                         const time = parseTime(event.args);
-                        bot.giveVoice(user, event.channel)
+                        bot.giveVoice(target, event.channel)
                             .then(() => {
-                                addTimer(time, 'devoice', user, event.channel);
+                                addTimer(time, 'devoice', target, event.channel);
 
-                                bot.notify(event.nick, `User ${user} was voiced for ${time.toNow(true)}.`);
+                                bot.notify(event.nick, `${target} was voiced for ${time.toNow(true)}.`);
                             })
                             .catch((error) => {
-                                bot.notify(event.nick, `Could not voice user: ${error}`);
+                                bot.notify(event.nick, `Could not voice ${target}: ${error}`);
                             });
                     }
                 },
-                'Usage: voice [USER_NAME]'
+                'Usage: voice [TARGET_NICK]'
             ],
             devoice: [
                 {
                     pattern: /^(\S*)$/,
                     requires: 'operator',
-                    execute: (event, user) => {
-                        if (user.length === 0) user = event.nick;
-                        bot.takeVoice(user, event.channel)
+                    execute: (event, target) => {
+                        if (target.length === 0) target = event.nick;
+                        bot.takeVoice(target, event.channel)
                             .then(() => {
-                                bot.notify(event.nick, `User ${user} was devoiced.`);
+                                bot.notify(event.nick, `${target} was devoiced.`);
                             })
                             .catch((error) => {
-                                bot.notify(event.nick, `Could not devoice user: ${error}`);
+                                bot.notify(event.nick, `Could not devoice ${target}: ${error}`);
                             });
                     }
                 },
-                'Usage: devoice [USER_NAME]'
+                'Usage: devoice [TARGET_NICK]'
             ],
             quiet: [
                 {
@@ -206,7 +206,7 @@ module.exports = (bot, config) => {
                             });
                     }
                 },
-                'Usage: quiet [USER_NAME]'
+                'Usage: quiet [TARGET_NICK]'
             ],
             unquiet: [
                 {
@@ -225,7 +225,7 @@ module.exports = (bot, config) => {
                             });
                     }
                 },
-                'Usage: unquiet [USER_NAME]'
+                'Usage: unquiet [TARGET_NICK]'
             ],
             kick: [
                 {
@@ -234,12 +234,12 @@ module.exports = (bot, config) => {
                     execute: (event, target) => {
                         bot.users.get(target)
                             .then((whois) => {
-                                if(bot.users.isInChannel(target, event.channel)){
+                                if (bot.users.isInChannel(target, event.channel)) {
                                     bot.giveOp(bot.client.nick, event.channel)
                                         .then(() => bot.kick(target, event.channel))
                                         .then(() => bot.takeOp(bot.client.nick, event.channel));
-                                }else{
-                                    throw ('User not in channel');
+                                } else {
+                                    throw 'Not in channel';
                                 }
 
                             })
@@ -251,7 +251,7 @@ module.exports = (bot, config) => {
                             });
                     }
                 },
-                'Usage: kick [USER_NAME]'
+                'Usage: kick [TARGET_NICK]'
             ]
         }
     };
